d66e405bb6ca3c82350c5fa159f175ed9705a263
lym0302 <lym0302@foxmail.com>
2025-04-08 10:36:09 +0800

    offload_path

--- third_party/VideoLLaMA2/videollama2/model/__init__.py
+++ third_party/VideoLLaMA2/videollama2/model/__init__.py
@@ -190,6 +190,11 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
         tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True, token=token)
         model = AutoModelForCausalLM.from_pretrained(model_path, config=config, **kwargs)
 
+    from accelerate import infer_auto_device_map, disk_offload
+    device_map = infer_auto_device_map(model, max_memory={"cpu": "max"} )
+    offload_path = os.path.abspath("./offload_folder")
+    disk_offload(model, folder=offload_path, device_map=device_map)
+
     processor = None
 
     if "videollama" in model_type:
